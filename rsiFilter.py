7a11c3a237a4060dc8e9fb4482daad203fefbd1f
declankra <declankramper@gmail.com>
2024-07-10 13:26:12 -0500

    changed back to single processing

--- rsiFilter.py
+++ rsiFilter.py
@@ -1,6 +1,7 @@
-import json
 import yfinance as yf
 import pandas as pd
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
 
 def calculate_rsi(data, period): # rsi value from google sheet OR 14 day period by default
     delta = data['Close'].diff(1)
@@ -15,7 +16,7 @@ def fetch_and_calculate_rsi(symbols, period, threshold):
     for symbol in symbols:
         data = yf.download(symbol, period="1mo")
         if not data.empty:
-            rsi = calculate_rsi(data, period = period)
+            rsi = calculate_rsi(data, period)
             rsi_last = rsi.iloc[-1]
             if rsi_last > threshold:  # Check if RSI is above the threshold
                 rsi_values[symbol] = rsi_last
