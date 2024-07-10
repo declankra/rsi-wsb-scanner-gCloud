f94da2ddc6d76027e08201a1b6131ac9c99cd2ec
declankra <declankramper@gmail.com>
2024-07-10 10:53:10 -0500

    initializing rsiFilter

--- rsiFilter.py
+++ rsiFilter.py
@@ -9,3 +9,6 @@ def calculate_rsi(data, period=14): # 14 day rsi value
     rs = avg_gain / avg_loss
     rsi = 100 - (100 / (1 + rs))
     return rsi
+
+def rsiFilter(all_filtered_stocks, rsiPeriod, rsiThreshold):
+    return
